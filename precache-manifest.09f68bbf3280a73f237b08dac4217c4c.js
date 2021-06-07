134de0968df975f18dfc183b190426bb4d0469d8
tiendd <tiendd@tmh-techlab.vn>
2021-06-07 11:19:01 +0700

    Updates

---
+++ precache-manifest.09f68bbf3280a73f237b08dac4217c4c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3984d1af31fdb9d75a5ddd5454db319a",
+    "url": "/react-image-editor/index.html"
+  },
+  {
+    "revision": "a1b358a85ec1a9267380",
+    "url": "/react-image-editor/static/css/2.3cd28122.chunk.css"
+  },
+  {
+    "revision": "269a7a0eb5790de1e8fc",
+    "url": "/react-image-editor/static/css/main.d18dee0f.chunk.css"
+  },
+  {
+    "revision": "a1b358a85ec1a9267380",
+    "url": "/react-image-editor/static/js/2.d684d38f.chunk.js"
+  },
+  {
+    "revision": "da6e65d0c775835d2580fbf0c330695d",
+    "url": "/react-image-editor/static/js/2.d684d38f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "269a7a0eb5790de1e8fc",
+    "url": "/react-image-editor/static/js/main.4c7bb062.chunk.js"
+  },
+  {
+    "revision": "5b37ce5088e8a2cfa921",
+    "url": "/react-image-editor/static/js/runtime-main.17fe7105.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/react-image-editor/static/media/logo.8dad8028.svg"
+  }
+]);
